b4c9166bee246af0fa7d2ce71cfafc9f57a7f4f1
digu-d <diguguido13@gmail.com>
2025-11-16 11:21:57 -0300

    Fix game logic for guessing number

--- 2essaywhile.py
+++ 2essaywhile.py
@@ -6,15 +6,14 @@ chances = int(input("Quantas chances voce gostaria de possuir: "))
 
 numero_secreto = random.randint(1, limite)
 
-acertou = False
 
 while chances > 0:
     print("Voce tem: ",chances, "chances")
     tentativa = int(input("Tente adivinhar o número secreto: "))
 
     if tentativa == numero_secreto:
-        print("Parabens Voce acertou fim de jogo")
-        acertou = True
+        print("Parabens Voce acertou fim de jogo") 
+        chances = -1
     elif tentativa > numero_secreto:
         print("O numero secreto e menor")
     else:
@@ -22,6 +21,7 @@ while chances > 0:
 
     chances -= 1
 
-if not acertou:
+if chances == 0:
     print("Suas chances acabaram")
     print("O numero secreto era: ", numero_secreto)
+
